f8316a2e9c02558e1ec2a464af575871844c4932
Itz-kwaz <nkwachinwamaghinna@gmail.com>
2020-10-27 16:00:35 +0100

    fixes to home screen

--- lib/UI/homeScreen.dart
+++ lib/UI/homeScreen.dart
@@ -61,18 +61,26 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             Center(
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Placeholder(
                     color: Colors.yellow.shade600,
-                    fallbackHeight:MediaQuery.of(context).size.width * 0.3,
-                    fallbackWidth: MediaQuery.of(context).size.width * 0.3
+                    fallbackHeight:MediaQuery.of(context).size.width * 0.25,
+                    fallbackWidth: MediaQuery.of(context).size.width * 0.25
                   ),
                   SizedBox(
                     width: 8.0,
                   ),
+                  Text('20',
+                  style: GoogleFonts.muli(
+                    textStyle: Theme.of(context).textTheme.headline4,
+                    color: kTextColor
+                  ),)
                 ],
               ),
+            ),
+            SizedBox(
+              height: 16.0,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -91,65 +99,74 @@ class _HomeScreenState extends State<HomeScreen> {
                   Text('Today',
                   style: GoogleFonts.muli(
                     color: kTextColor,
-                    textStyle: Theme.of(context).textTheme.subtitle2
-                  ),),
-                  Text('Next 7 Days',
-                  style: GoogleFonts.muli(
                     textStyle: Theme.of(context).textTheme.subtitle2,
-                    color: kTextColor
-                  ),)
+                      fontWeight: FontWeight.w700
+                  ),),
+                  Row(
+                    children: [
+                      Text('Next 7 Days',
+                      style: GoogleFonts.muli(
+                        textStyle: Theme.of(context).textTheme.subtitle2,
+                        color: kTextColor,
+                        fontWeight: FontWeight.w700
+                      ),),
+                      Icon(Icons.keyboard_arrow_right_rounded,
+                      color: kTextColor,)
+                    ],
+                  )
                 ],
             ),
             SizedBox(
               height: 8.0,
             ),
-            ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  padding: EdgeInsets.all(8.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8.0),
-                    border: Border.all(
-                      color: Colors.grey.shade400,
-                      width: 1.0
-                    )
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        child: Text(
-                          '10:00',
-                          style: GoogleFonts.muli(
-                            textStyle: Theme.of(context).textTheme.subtitle1,
-                            color: Colors.grey
+            Expanded(
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    padding: EdgeInsets.all(8.0),
+                    margin: EdgeInsets.symmetric(horizontal: 8.0,vertical: 8.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      border: Border.all(
+                        color: Colors.grey.shade400,
+                        width: 1.0
+                      )
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            '10:00',
+                            style: GoogleFonts.muli(
+                              textStyle: Theme.of(context).textTheme.subtitle1,
+                              color: Colors.grey
+                            ),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 8.0,
-                      ),
-                      Expanded(child: Placeholder(
-                        color: Colors.green,
-                      )),
-                      SizedBox(
-                        height: 8.0,
-                      ),
-                      Expanded(
-                          child: Text('20',
-                          style: GoogleFonts.muli(
-                            textStyle: Theme.of(context).textTheme.subtitle1,
-                            color: kTextColor,
-                            fontWeight: FontWeight.bold
-                          ),)
-                      )
-                    ],
-                  ),
-                );
-              },),
+                        Expanded(child: Center(
+                          child: Placeholder(
+                            color: Colors.green,
+                            fallbackWidth: 40.0,
+                          ),
+                        )),
 
-
+                        Expanded(
+                            child: Center(
+                              child: Text('20',
+                              style: GoogleFonts.muli(
+                                textStyle: Theme.of(context).textTheme.subtitle1,
+                                color: kTextColor,
+                                fontWeight: FontWeight.bold
+                              ),),
+                            )
+                        )
+                      ],
+                    ),
+                  );
+                },),
+            ),
           ],
         ),
       ),
@@ -165,13 +182,21 @@ class MiniWidget extends StatelessWidget {
     return Column(
       children: [
         Placeholder(
-          fallbackWidth: MediaQuery.of(context).size.width  * 0.2,
-          fallbackHeight: MediaQuery.of(context).size.height * 0.2,
+          fallbackWidth: MediaQuery.of(context).size.width  * 0.06,
+          fallbackHeight: MediaQuery.of(context).size.height * 0.06,
           color: Colors.red,
         ),
         SizedBox(
           height: 8.0,
         ),
+        Text(
+          '9 km/h',
+          style: GoogleFonts.muli(
+            textStyle: Theme.of(context).textTheme.subtitle2,
+            fontWeight: FontWeight.bold,
+            color: kTextColor
+          ),
+        )
       ],
     );
   }
--- lib/UI/next7DaysScreen.dart
+++ lib/UI/next7DaysScreen.dart
@@ -12,76 +12,83 @@ class Next7DaysScreen extends StatelessWidget {
             child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-            Container(
-              decoration: BoxDecoration(
+            Material(
+              child: Container(
+                decoration: BoxDecoration(
 
-              ),
-              child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('Tomorrow',
-                        style: GoogleFonts.muli(
-                          color: kWhite,
-                          textStyle: Theme.of(context).textTheme.subtitle2
-                        ),),
-                        Text('Sun,7 Aug',
+                ),
+                child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Tomorrow',
                           style: GoogleFonts.muli(
-                              color: kWhite,
-                              textStyle: Theme.of(context).textTheme.caption
-                          ),)
-                      ],
-                    ),
-                    SizedBox(
-                      height: 8.0,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Expanded(
-                            child: Placeholder(
-                          color: Colors.yellow,
-                        ),
-                        flex: 2,),
-                        Expanded(
-                          flex: 1,
-                          child: Column(
-                            children: [
-                              Text('20',
-                              style: GoogleFonts.muli(
-                                textStyle: Theme.of(context).textTheme.headline4,
-                                color: kWhite,
-                                fontWeight: FontWeight.bold
-                              )),
-                              SizedBox(
-                                height: 8.0,
-                              ),
-                              Text('Sunny',
-                              style: GoogleFonts.muli(
+                            color: kWhite,
+                            textStyle: Theme.of(context).textTheme.subtitle2
+                          ),),
+                          Text('Sun,7 Aug',
+                            style: GoogleFonts.muli(
                                 color: kWhite,
                                 textStyle: Theme.of(context).textTheme.caption
-                              ),)
-                            ],
+                            ),)
+                        ],
+                      ),
+                      SizedBox(
+                        height: 8.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Expanded(
+                              child: Placeholder(
+                            color: Colors.yellow,
                           ),
-                        ),
-                        Expanded(child: Column(
-                          children: [
-                            Text('/ 17',
-                            style: GoogleFonts.muli(
-                              textStyle: Theme.of(context).textTheme.subtitle1,
-                              color: kWhite,
+                          flex: 2,),
+                          Expanded(
+                            flex: 1,
+                            child: Column(
+                              children: [
+                                Text('20',
+                                style: GoogleFonts.muli(
+                                  textStyle: Theme.of(context).textTheme.headline4,
+                                  color: kWhite,
+                                  fontWeight: FontWeight.bold
+                                )),
+                                SizedBox(
+                                  height: 8.0,
+                                ),
+                                Text('Sunny',
+                                style: GoogleFonts.muli(
+                                  color: kWhite,
+                                  textStyle: Theme.of(context).textTheme.caption
+                                ),)
+                              ],
+                            ),
+                          ),
+                          Expanded(child: Column(
+                            children: [
+                              Text('/ 17',
+                              style: GoogleFonts.muli(
+                                textStyle: Theme.of(context).textTheme.subtitle1,
+                                color: kWhite,
 
-                            ),),
-                          ],
-                        ))
+                              ),),
+                            ],
+                          ))
 
-                      ],
-                    )
+                        ],
+                      )
 
-                  ],
+                    ],
+                ),
               ),
-            )
+              elevation: 2.0,
+            ),
+          Expanded(child: ListView.builder(itemBuilder: (context,index){
+            return Container();
+          }))
+
         ],
       ),
           )),
